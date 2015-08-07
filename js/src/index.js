c7f8bcdb72675f34b563334cbc4a75b3dde746ee
gabrielgiussi <gabrielgiussi@gmail.com>
2015-08-07 09:20:26 -0300

    Update index.js

--- js/src/index.js
+++ js/src/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import App from './common/App';
 
-React.render(<App/>,document.getElementById('casacasapp'));
+React.render(<App/>,document.getElementById('actionstore'));
